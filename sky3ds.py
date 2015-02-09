37478c6fca81297263045a35cf9dfab99f03dcd7
Lukas Schauer <lukas@schauer.so>
2015-02-09 15:06:25 +0100

    always show rom list when available

--- sky3ds.py
+++ sky3ds.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--disk', help='Sky3DS disk')
 
-    parser.add_argument('-l', '--list', help='List roms on disk', action="store_true")
+    parser.add_argument('-l', '--list', help='List roms on disk (default operation)', action='store_true')
     parser.add_argument('-w', '--write', help='Write rom to disk')
     parser.add_argument('-b', '--backup', help='Backup rom from disk')
     parser.add_argument('-r', '--remove', help='Remove rom from disk')
@@ -54,8 +54,8 @@ if __name__ == '__main__':
 
     disk = disk.Sky3DS_Disk(args.disk)
 
-    if (args.backup != None) + (args.write != None) + (args.remove != None) + (args.backup_savegame != None) + (args.write_savegame != None) + args.list + args.format + args.update != 1:
-        print("Please specify exactly one operation.")
+    if (args.backup != None) + (args.write != None) + (args.remove != None) + (args.backup_savegame != None) + (args.write_savegame != None) + args.format + args.update > 1:
+        print("Please specify only one operation.")
         sys.exit(1)
 
     if args.format:
@@ -63,7 +63,6 @@ if __name__ == '__main__':
             print("Please confirm format operation with '-c'.")
             sys.exit(1)
         disk.format()
-        sys.exit(0)
 
     if not args.update and not disk.is_sky3ds_disk:
         print("This is not a sky3ds disk. Aborting.")
@@ -74,68 +73,59 @@ if __name__ == '__main__':
         if args.remove in [i[0] for i in disk.rom_list]:
             disk.delete_rom(args.remove)
             print("Removed rom from slot %d" % args.remove)
-        sys.exit(0)
-
-    if args.list:
-        rom_table = [['Slot', 'Start', 'Size', 'Type', 'Code', 'Title', 'Save Crypto']]
-        for rom in disk.rom_list:
-            slot = rom[0]
-            start = rom[1]
-            size = rom[2]
-            rom_header = disk.ncsd_header(slot)
-            rom_info = titles.rom_info(rom_header['product_code'], rom_header['media_id'])
-            if rom_info:
-                title = rom_info['name']
-                firmware = rom_info['firmware']
-            else:
-                title = "???"
-                firmware = "???"
-            rom_table += [[
-                slot,
-                "%d MB" % int(rom[1] / 1024 / 1024),
-                "%d MB" % int(rom[2] / 1024 / 1024),
-                rom_header['card_type'],
-                rom_header['product_code'],
-                title,
-                rom_header['save_crypto'].rjust(12),
-                #firmware,
-                #'Yes' if rom_header['contains_update'] else 'No',
-                ]]
-
-        col_width = [max(len(str(x)) for x in col) for col in zip(*rom_table)]
-        for line in rom_table:
-            print("| " + " | ".join("{:{}}".format(x, col_width[i]) for i, x in enumerate(line)) + " |")
-
-        print("")
-        total_free_blocks = sum(512*i[1] for i in disk.free_blocks)
-
-        print("Disk Size: %d MB | Free space: %d MB | Largest free continous space: %d MB" % (disk.disk_size/1024/1024, total_free_blocks/1024/1024, 512 * disk.free_blocks[0][1]/1024/1024))
-        sys.exit(0)
 
     if args.update:
         titles.update_title_db()
-        sys.exit(0)
 
     if args.backup != None and args.slot == None:
         print("Please specify slot")
         sys.exit(1)
     elif args.backup != None and args.slot != None:
         disk.dump_rom(int(args.slot), args.backup)
-        sys.exit(0)
 
     if args.backup_savegame != None and args.slot == None:
         print("Please specify slot")
         sys.exit(1)
     elif args.backup_savegame != None and args.slot != None:
         disk.dump_savegame(int(args.slot), args.backup_savegame)
-        sys.exit(0)
 
     if args.write_savegame != None:
         disk.write_savegame(args.write_savegame)
-        sys.exit(0)
 
     if args.write != None:
         disk.write_rom(args.write)
-        sys.exit(0)
 
+    rom_table = [['Slot', 'Start', 'Size', 'Type', 'Code', 'Title', 'Save Crypto']]
+    for rom in disk.rom_list:
+        slot = rom[0]
+        start = rom[1]
+        size = rom[2]
+        rom_header = disk.ncsd_header(slot)
+        rom_info = titles.rom_info(rom_header['product_code'], rom_header['media_id'])
+        if rom_info:
+            title = rom_info['name']
+            firmware = rom_info['firmware']
+        else:
+            title = "???"
+            firmware = "???"
+        rom_table += [[
+            slot,
+            "%d MB" % int(rom[1] / 1024 / 1024),
+            "%d MB" % int(rom[2] / 1024 / 1024),
+            rom_header['card_type'],
+            rom_header['product_code'],
+            title,
+            rom_header['save_crypto'].rjust(12),
+            #firmware,
+            #'Yes' if rom_header['contains_update'] else 'No',
+            ]]
+
+    col_width = [max(len(str(x)) for x in col) for col in zip(*rom_table)]
+    for line in rom_table:
+        print("| " + " | ".join("{:{}}".format(x, col_width[i]) for i, x in enumerate(line)) + " |")
+
+    print("")
+    total_free_blocks = sum(512*i[1] for i in disk.free_blocks)
+
+    print("Disk Size: %d MB | Free space: %d MB | Largest free continous space: %d MB" % (disk.disk_size/1024/1024, total_free_blocks/1024/1024, 512 * disk.free_blocks[0][1]/1024/1024))
 
